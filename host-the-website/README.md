d8a4d62928b93fa92ac5c99f906dc772a659d0f1
neilgri@gmail.com <neilgri@gmail.com>
2023-07-19 15:06:21 +0200

    host the website

---
+++ host-the-website/README.md
@@ -0,0 +1,45 @@
+# Azure Static Website Deployment Script
+
+This script is used to deploy a static website to Azure using PowerShell. 
+
+## Variables
+
+Here are the variables that need to be set before executing the script:
+
+- `$appId`: The name of your application. (Example: "widget")
+- `$subscriptionId`: The ID of your Azure subscription. (Example: "aaaaaaaa-bbbb-cccc-dddd-eeeeeeeeeeee")
+- `$location`: The location of your Azure resources. (Example: "West US")
+- `$pathToFiles`: The path to the website files you want to upload. (Example: "C:\Users\Username\Documents\Website")
+
+## How it works
+
+The script first sets up the required Azure resources, which include a Resource Group and a Storage Account. It then enables static website hosting on the Storage Account and uploads the website files to the `$web` container. Once the files are uploaded, the script fetches the primary endpoint for the static website and opens it in your default browser.
+
+## How to use
+
+1. Install the [Azure PowerShell module](https://docs.microsoft.com/en-us/powershell/azure/install-az-ps) if it's not already installed.
+
+2. Open PowerShell and navigate to the directory containing the script.
+
+3. Set the variables mentioned above.
+
+4. Run the script using the command `.\script_name.ps1`, replacing `script_name` with the actual name of your script file.
+
+Please note that you must have the appropriate permissions to create and manage resources in your Azure subscription. Also, the script assumes that you're using the 'Standard_RAGRS' redundancy option for the Storage Account, and that all HTML, CSS, and JS files in your local directory should be uploaded to the Storage Account. Make sure to modify these options as needed to suit your use case.
+
+## Output
+
+The script will output the primary endpoint for the static website, which is the URL where your website is hosted. You can share this URL with others to allow them to access your website.
+
+If any part of the script fails, the script will stop execution and display a message indicating which part failed.
+
+## Further Improvements
+
+This script is quite basic and may not suit all use cases. Here are some improvements you might want to consider:
+
+- Implement error checking for each command to ensure that it completed successfully.
+- Modify the script to suit your coding and infrastructure standards.
+- Parameterize the script to make it reusable for different applications or environments.
+- Incorporate the script into your CI/CD pipeline for automated deployments.
+
+Please remember that this script may incur costs on your Azure subscription, depending on the resources that you create and use. Always monitor your usage and costs to avoid unexpected charges.
---
+++ host-the-website/script.ps1
@@ -0,0 +1,137 @@
+# Variables
+$appId = "" # Example: "widget"
+$subscriptionId = "" # Example: "aaaaaaaa-bbbb-cccc-dddd-eeeeeeeeeeee"
+$location = ""  # Example: "West US"
+$pathToFiles = ""  # Example: "C:\Users\Username\Documents\Website"
+
+# Automated values based on variables
+$resourceGroupName = $appId + "-rg" # Creating a name for the resource group
+$storageAccountNamePrefix = $appId # Creating a prefix for the storage account name
+
+# Function to generate a random string
+function Get-RandomString($length = 8) {
+    return -join ((65..90) + (97..122) | Get-Random -Count $length | % {[char]$_})
+}
+
+# Installing the Azure PowerShell module if not installed
+if (!(Get-Module -ListAvailable -Name Az.Accounts)) {
+    Install-Module -Name Az -AllowClobber -Scope CurrentUser
+}
+
+# Import the module
+Import-Module Az.Accounts
+
+Write-Host "Logging in to Azure..."
+
+# Login to your Azure account
+Connect-AzAccount
+
+if (-not $?) {
+    Write-Host "Failed to log in to Azure. Stopping script."
+    return
+}
+
+Write-Host "Setting subscription context..."
+
+# Setting the subscription
+Set-AzContext -SubscriptionId $subscriptionId
+
+if (-not $?) {
+    Write-Host "Failed to set Azure context. Stopping script."
+    return
+}
+
+Write-Host "Creating resource group..."
+
+# Creating the Resource Group
+New-AzResourceGroup -Name $resourceGroupName -Location $location
+
+if (-not $?) {
+    Write-Host "Failed to create resource group. Stopping script."
+    return
+}
+
+Write-Host "Creating storage account..."
+
+# Creating the Storage Account with unique name
+$storageAccountName = $storageAccountNamePrefix + (Get-RandomString -length 8).ToLower()
+$storageAccount = New-AzStorageAccount -ResourceGroupName $resourceGroupName -Name $storageAccountName -Location $location -SkuName Standard_RAGRS -Kind StorageV2
+
+if (-not $?) {
+    Write-Host "Failed to create storage account. Stopping script."
+    return
+}
+
+Write-Host "Waiting for storage account to be ready..."
+
+# Check if the storage account is ready
+do {
+    Start-Sleep -Seconds 10
+    $storageAccount = Get-AzStorageAccount -ResourceGroupName $resourceGroupName -Name $storageAccountName -ErrorAction SilentlyContinue
+} while ($null -eq $storageAccount)
+
+Write-Host "Getting storage account key..."
+
+# Getting the Storage Account Key
+$storageKey = (Get-AzStorageAccountKey -ResourceGroupName $resourceGroupName -Name $storageAccountName).Value[0]
+
+if (-not $?) {
+    Write-Host "Failed to get storage account key. Stopping script."
+    return
+}
+
+Write-Host "Setting storage context..."
+
+# Setting the Storage Context
+$ctx = New-AzStorageContext -StorageAccountName $storageAccountName -StorageAccountKey $storageKey
+
+if (-not $?) {
+    Write-Host "Failed to set storage context. Stopping script."
+    return
+}
+
+# Enabling static website hosting
+Enable-AzStorageStaticWebsite -Context $ctx -IndexDocument index.html
+
+if (-not $?) {
+    Write-Host "Failed to enable static website on storage account. Stopping script."
+    return
+}
+
+Write-Host "Uploading files to the web container..."
+
+# Uploading files to the web container with conditional content type
+Get-ChildItem -Path $pathToFiles -Recurse |
+ForEach-Object {
+    $blobName = $_.FullName.Substring($pathToFiles.Length + 1).Replace('\', '/')
+    if ($_.Extension -eq ".html") {
+        Set-AzStorageBlobContent -File $_.FullName -Blob $blobName -Container `$web -Context $ctx -Properties @{"ContentType" = "text/html"} -Force
+    }
+    if ($_.Extension -eq ".css") {
+        Set-AzStorageBlobContent -File $_.FullName -Blob $blobName -Container `$web -Context $ctx -Properties @{"ContentType" = "text/css"} -Force
+    }
+    if ($_.Extension -eq ".js") {
+        Set-AzStorageBlobContent -File $_.FullName -Blob $blobName -Container `$web -Context $ctx -Properties @{"ContentType" = "application/javascript"} -Force
+    } 
+}
+
+if (-not $?) {
+    Write-Host "Failed to upload files to the web container. Stopping script."
+    return
+}
+
+
+# Get the primary static website endpoint
+$primaryWebEndpoint = $storageAccount.PrimaryEndpoints.Web
+
+Write-Host "The primary endpoint for the static website is: $primaryWebEndpoint"
+
+# Wait for the web content to be loaded
+Write-Host "Waiting for the web content to be ready..."
+start-sleep -Seconds 15
+
+# Open the primary endpoint in the default browser
+Write-Host "Opening the site in the default browser"
+Start-Process $primaryWebEndpoint
+
+Write-Host "Script end"
