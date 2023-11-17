d0830ddc8160cf4b513d5cac49b33909eec7d6fc
neilgri@gmail.com <neilgri@gmail.com>
2023-11-17 14:32:27 +0100

    cdn

--- chunk 1 - frontend/05. Secure with HTTPS/azure/terraform/res-cdn.tf
+++ chunk 1 - frontend/05. Secure with HTTPS/azure/terraform/res-cdn.tf
@@ -1,7 +1,7 @@
 # Create an Azure CDN profile
 // https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/cdn_profile
 resource "azurerm_cdn_profile" "this" {
-  name                = "${lower(var.environment)}${lower(random_string.this.result)}-cnd-profile"
+  name                = "${lower(var.environment)}${lower(random_string.this.result)}-cdn-profile"
   location            = var.location
   resource_group_name = azurerm_resource_group.this.name
   sku                 = "Standard_Microsoft"
@@ -10,7 +10,7 @@ resource "azurerm_cdn_profile" "this" {
 # Create an Azure CDN endpoint
 // https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/cdn_endpoint
 resource "azurerm_cdn_endpoint" "this" {
-  name                = "${lower(var.environment)}${lower(random_string.this.result)}-cnd-endpoint"
+  name                = "${lower(var.environment)}${lower(random_string.this.result)}-cdn-endpoint"
   profile_name        = azurerm_cdn_profile.this.name
   location            = var.location
   resource_group_name = azurerm_resource_group.this.name
@@ -18,10 +18,10 @@ resource "azurerm_cdn_endpoint" "this" {
   is_https_allowed    = true
 
   # Extract the domain name from the primary web endpoint and remove the trailing slash
-  origin_host_header  = trimsuffix(replace(azurerm_storage_account.this.primary_web_endpoint, "https://", ""), "/")
+  origin_host_header = trimsuffix(replace(azurerm_storage_account.this.primary_web_endpoint, "https://", ""), "/")
 
   origin {
-    name      = "storage-account-origin"
+    name = "storage-account-origin"
     # Extract the domain name from the primary web endpoint and remove the trailing slash
     host_name = trimsuffix(replace(azurerm_storage_account.this.primary_web_endpoint, "https://", ""), "/")
   }
